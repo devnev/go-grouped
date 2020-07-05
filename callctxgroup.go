7640b40bb28c8289bcb935b47e3b92b38e82d5e0
Mark Nevill <mark.nevill@gmail.com>
2020-07-05 19:26:13 +0100

    Cleanup

--- callctxgroup.go
+++ callctxgroup.go
@@ -1,78 +1,24 @@
 package sync2
 
-import (
-	"context"
-	"sync"
-)
+import "context"
 
 type CallCtxGroup struct {
-	mu     sync.Mutex
-	groups map[string]*callCtxGroupInner
+	callGroup CallGroup
 }
 
-type callCtxGroupInner struct {
-	leader   chan struct{}
-	done     chan struct{}
-	result   interface{}
-	err      error
-	monitors int
+type callResult struct {
+	val interface{}
+	err error
 }
 
 func (g *CallCtxGroup) Do(ctx context.Context, key string, do func() (interface{}, error)) (interface{}, GroupResult, error) {
-	g.mu.Lock()
-	if g.groups == nil {
-		g.groups = make(map[string]*callCtxGroupInner)
+	res, grouped := g.callGroup.Do(key, ctx.Done(), func() (interface{}, bool) {
+		val, err := do()
+		return callResult{val: val, err: err}, ctx.Err() == nil
+	})
+	if grouped == GroupCanceled {
+		return nil, GroupCanceled, ctx.Err()
 	}
-	if g.groups[key] == nil {
-		g.groups[key] = &callCtxGroupInner{
-			leader: make(chan struct{}, 1),
-			done:   make(chan struct{}),
-		}
-		g.groups[key].leader <- struct{}{}
-	}
-	inner := g.groups[key]
-	inner.monitors++
-	g.mu.Unlock()
-
-	select {
-	case <-ctx.Done():
-		g.mu.Lock()
-		defer g.mu.Unlock()
-		if inner != g.groups[key] {
-			return inner.result, GroupShared, inner.err
-		} else {
-			inner.monitors--
-			return nil, GroupCanceled, ctx.Err()
-		}
-	case <-inner.done:
-		return inner.result, GroupShared, inner.err
-	case <-inner.leader:
-	}
-
-	// pass on leadership unless we accept the result
-	accepted := false
-	defer func() {
-		if !accepted {
-			inner.leader <- struct{}{}
-		}
-	}()
-	if result, err := do(); ctx.Err() != nil {
-		// if the context is done we assume the result was affected by this and so the result is
-		// exclusive to this call rather than relevant to the entire group.
-		return result, GroupExclusive, err
-	} else {
-		inner.result = result
-		inner.err = err
-	}
-	accepted = true
-
-	g.mu.Lock()
-	delete(g.groups, key)
-	g.mu.Unlock()
-	close(inner.done)
-	if inner.monitors > 1 {
-		return inner.result, GroupShared, inner.err
-	} else {
-		return inner.result, GroupExclusive, inner.err
-	}
+	res2 := res.(callResult)
+	return res2.val, grouped, res2.err
 }
